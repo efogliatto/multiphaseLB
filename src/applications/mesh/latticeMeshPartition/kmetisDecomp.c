a5648cfad37d386811ad16a73da0661fc8fc9d46
Ezequiel O. Fogliatto <ezefogliatto@gmail.com>
2017-07-10 11:33:18 -0300

    Correcci\'on a kmetisDecomp para el caso en que worldSize = 1

--- src/applications/mesh/latticeMeshPartition/kmetisDecomp.c
+++ src/applications/mesh/latticeMeshPartition/kmetisDecomp.c
@@ -8,109 +8,127 @@
 void kmetisDecomp( unsigned int* owner, struct basicMesh* mesh, unsigned int np )  {
 
 
-    // Create graph for metis decomposition.
+    if( np > 1 ) {
+
+	// Create graph for metis decomposition.
 
     
-    // Count total number of edges (counted twice)
+	// Count total number of edges (counted twice)
 
-    unsigned int nedges = 0;
+	unsigned int nedges = 0;
 
-    unsigned int i, k;
+	unsigned int i, k;
 
-    for( i = 0 ; i < mesh->nPoints ; i++ ) {
-
-	for( k = 1 ; k < mesh->Q ; k++ ) {
-
-	    if( mesh->nb[i][k] != -1 ) {
-
-		nedges++;
-
-	    }
-
-	}
-
-    }
-
-
-    nedges = nedges / 2;
-
-
-
-    // Write graph file
-
-    FILE* gfile = fopen("lattice/lattice.graph","w");
-
-    
-    fprintf(gfile,"%d %d\n", mesh->nPoints, nedges);
-
-    for( i = 0 ; i < mesh->nPoints ; i++ ) {
-
-	for( k = 1 ; k < mesh->Q ; k++ ) {
-
-	    if( mesh->nb[i][k] != -1 ) {
-
-		fprintf(gfile,"%d ", mesh->nb[i][k]+1);
-
-	    }
-
-	}
-
-	fprintf(gfile,"\n");
-
-    }
-    
-    
-    fclose(gfile);
-
-    
-
-
-
-
-    // Apply metis algorithm. Call external function gpmetis
-
-    char cmd[100];
-
-    sprintf(cmd,"gpmetis lattice/lattice.graph %d > log.gpmetis",np);
-
-    unsigned int status = system( cmd );
-
-
-    if (!status) {
-
-
-	// Read gpmetis result and load into owner
-
-	sprintf(cmd,"lattice/lattice.graph.part.%d",np);
-
-	gfile = fopen(cmd,"r");
-
-	
 	for( i = 0 ; i < mesh->nPoints ; i++ ) {
 
-	    fscanf(gfile,"%d",&owner[i]);
+	    for( k = 1 ; k < mesh->Q ; k++ ) {
+
+		if( mesh->nb[i][k] != -1 ) {
+
+		    nedges++;
+
+		}
+
+	    }
 
 	}
-	
-	
+
+
+	nedges = nedges / 2;
+
+
+
+	// Write graph file
+
+	FILE* gfile = fopen("lattice/lattice.graph","w");
+
+    
+	fprintf(gfile,"%d %d\n", mesh->nPoints, nedges);
+
+	for( i = 0 ; i < mesh->nPoints ; i++ ) {
+
+	    for( k = 1 ; k < mesh->Q ; k++ ) {
+
+		if( mesh->nb[i][k] != -1 ) {
+
+		    fprintf(gfile,"%d ", mesh->nb[i][k]+1);
+
+		}
+
+	    }
+
+	    fprintf(gfile,"\n");
+
+	}
+    
+    
 	fclose(gfile);
+
+    
+
+
+
+
+	// Apply metis algorithm. Call external function gpmetis
+
+	char cmd[100];
+
+	sprintf(cmd,"gpmetis lattice/lattice.graph %d > log.gpmetis",np);
+
+	unsigned int status = system( cmd );
+
+
+	if (!status) {
+
+
+	    // Read gpmetis result and load into owner
+
+	    sprintf(cmd,"lattice/lattice.graph.part.%d",np);
+
+	    gfile = fopen(cmd,"r");
+
+	
+	    for( i = 0 ; i < mesh->nPoints ; i++ ) {
+
+		fscanf(gfile,"%d",&owner[i]);
+
+	    }
+	
+	
+	    fclose(gfile);
 	
 
-	/* status = system("rm lattice/lattice.graph*"); */
+	    /* status = system("rm lattice/lattice.graph*"); */
 
 	
+	}
+
+	else {
+
+	    printf("\n   [ERROR]  gpmetis not executed\n\n");
+
+	    exit(1);
+
+	}
+
+    
     }
+
+
+
+    
 
     else {
 
-	printf("\n   [ERROR]  gpmetis not executed\n\n");
+	unsigned int i;
 
-	exit(1);
+	for ( i = 0 ; i < mesh->nPoints ; i++ ) {
+
+	    owner[i] = 0;
+
+	}
 
     }
-
-    
-    
 
 }
 
