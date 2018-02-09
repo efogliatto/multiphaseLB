6fcf24d6ea1b7197f7ffa98989f61346849bbb6e
Ezequiel O. Fogliatto <ezefogliatto@gmail.com>
2018-02-09 08:42:03 -0300

    Correcci\'on en uniformHeatFlux para emplear myMRTModel.
    HAY QUE GENERALIZAR LA IMPLEMENTACI\'ON DE VALORES DE EQUILIBRIO

--- src/lbEquation/general/boundary/uniformHeatFlux.c
+++ src/lbEquation/general/boundary/uniformHeatFlux.c
@@ -4,14 +4,15 @@
 #include <lbeField.h>
 #include <macroFields.h>
 #include <liTempEquilibrium.h>
+#include <myMRTEquilibrium.h>
 
 
 void uniformHeatFlux( struct latticeMesh* mesh, struct macroFields* mfields, struct lbeField* field, unsigned int bid ) {
 
 
-    // Apply only if collision model is liTemp or newTemp
+    // Apply only if collision model is liTemp or myMRTModel
 
-    if(  ( field->colId == 2 ) ||  ( field->colId == 3 )  ){
+    if(  ( field->colId == 2 ) ||  ( field->colId == 4 )  ){
 
 	
 	unsigned int i, k;
@@ -35,13 +36,33 @@ void uniformHeatFlux( struct latticeMesh* mesh, struct macroFields* mfields, str
 
 	    // Compute thermal conductivity
 
-	    double lambda = ( field->tau - 0.5 ) * mfields->rho[id] * mesh->lattice.cs2 * mesh->EOS._Cv;
+	    double lambda = 0;
+
+	    if( field->colId == 2 ) {
+
+		lambda = ( field->tau - 0.5 ) * mfields->rho[id] * mesh->lattice.cs2 * mesh->EOS._Cv;
+
+	    }
+
+	    else {
+
+		if( field->colId == 4 ) {
+
+		    lambda = mfields->rho[id] * mesh->EOS._Cv * (1/field->Lambda[3] - 0.5) * (4.0 + 3.0 * field->alpha_1 + 2.0 * field->alpha_2) / 6.0;
+
+		}
+
+	    }
+
+	    
 
 
 
 	    // Move over lattice velocities. Check direction
 
 
+	    // Estimated boundary temperature
+	    
 	    double t_bd = 0;
 	    
 	    for( k = 0 ; k < mesh->mesh.Q ; k++ ) {
@@ -77,39 +98,91 @@ void uniformHeatFlux( struct latticeMesh* mesh, struct macroFields* mfields, str
 	    }
 
 
-	    
+	    if( field->colId == 2 ) {
 	
-	    // Move over lattice velocities
+		// Move over lattice velocities
 	    
-	    for( k = 0 ; k < mesh->mesh.Q ; k++ ) {
+		for( k = 0 ; k < mesh->mesh.Q ; k++ ) {
 
 
-		if ( mesh->mesh.nb[id][k] == -1 ) {
+		    if ( mesh->mesh.nb[id][k] == -1 ) {
 
 
-		    // Need density and velocity at neighbour (reverse) node
+			// Need density and velocity at neighbour (reverse) node
 		    
-		    int nbid = mesh->mesh.nb[id][ mesh->lattice.reverse[k] ];		    
+			int nbid = mesh->mesh.nb[id][ mesh->lattice.reverse[k] ];		    
 		    
-		    if( nbid != -1 ) {
+			if( nbid != -1 ) {
 
 			
-			liTempEquilibrium( &mesh->lattice, mfields->rho[nbid], mfields->U[nbid], f_eq_nb );
+			    liTempEquilibrium( &mesh->lattice, mfields->rho[nbid], mfields->U[nbid], f_eq_nb );
 		
-			eq_bnd = mesh->EOS._Cv * t_bd * f_eq_nb[k];
+			    eq_bnd = mesh->EOS._Cv * t_bd * f_eq_nb[k];
 			    
-			eq_nb  = mesh->EOS._Cv * mfields->T[nbid] * f_eq_nb[k];
+			    eq_nb  = mesh->EOS._Cv * mfields->T[nbid] * f_eq_nb[k];
 
 			
-			// Update distribution
+			    // Update distribution
 			
-			field->value[id][k] = eq_bnd + (field->value[nbid][k] - eq_nb);
+			    field->value[id][k] = eq_bnd + (field->value[nbid][k] - eq_nb);
+
+			}
+
+		
 
 		    }
 
+		}
+
+
+	    }
+
+
+
+	    else {
+
+		
+		// Move over lattice velocities
+	    
+		for( k = 0 ; k < mesh->mesh.Q ; k++ ) {
+
+
+		    if ( mesh->mesh.nb[id][k] == -1 ) {
+
+
+			// Need density and velocity at neighbour (reverse) node
+		    
+			int nbid = mesh->mesh.nb[id][ mesh->lattice.reverse[k] ];		    
+		    
+			if( nbid != -1 ) {
+
+			    
+			    // Equilibrium at neighbour node
+			    
+			    myMRTEquilibrium( &mesh->lattice, mfields->T[nbid], mfields->U[nbid], field->alpha_1, field->alpha_2, f_eq_nb );
+			    
+			    eq_nb  = f_eq_nb[k];
+
+
+			    // Equilibrium at boundary node
+
+			    myMRTEquilibrium( &mesh->lattice, t_bd, mfields->U[nbid], field->alpha_1, field->alpha_2, f_eq_nb );
+			    
+			    eq_bnd = f_eq_nb[k];
+			    
+
+			
+			    // Update distribution
+			
+			    field->value[id][k] = eq_bnd + (field->value[nbid][k] - eq_nb);
+
+			}
 		
 
+		    }
+
 		}
+
 
 	    }
 
