442ef5f4c111623d6e726782fc4f8c1f6ba2ed7b
Ezequiel O. Fogliatto <ezefogliatto@gmail.com>
2018-03-09 14:12:27 -0300

    Correcci\'on de la condici\'on ouflow: interpolaci\'on a primer orden s\'olo de las componentes faltantes. C\'alculo interno de la direcci\'on normal usando los vecinos ausentes

--- src/lbEquation/general/boundary/outflow.c
+++ src/lbEquation/general/boundary/outflow.c
@@ -7,91 +7,9 @@
 #include <liMRTEquilibrium.h>
 
 
-/* void outflow( struct latticeMesh* mesh, struct macroFields* mfields, struct lbeField* field, unsigned int bid ) { */
-
-    
-/*     unsigned int i, j, k; */
-
-/*     double* f_eq_nb = (double*)malloc( mesh->mesh.Q * sizeof(double) ); */
-
-/*     double* f_eq_bnd  = (double*)malloc( mesh->mesh.Q * sizeof(double) ); */
-	
-
-    
-/*     // Velocity at boundary */
-
-/*     double Uw[3]; */
-
-    
-    
-/*     // Move over boundary elements */
-
-/*     for( i = 0 ; i < mesh->mesh.bd.nbdelem[bid] ; i++ ) { */
-
-	
-/* 	unsigned int id = mesh->mesh.bd.bdPoints[bid][i]; */
-
-	
-/* 	// Move over principal directions only */
-
-/* 	for( j = 1 ; j <= 4 ; j++ ) { */
-	
-
-/* 	    // Copy from reverse point if neighbour is not present */
-	    
-/* 	    if( mesh->mesh.nb[id][j] == -1 ) { */
-
-		
-/* 		Uw[0] = mfields->U[ mesh->mesh.nb[id][mesh->lattice.reverse[j]] ][0]; */
-/* 		Uw[1] = mfields->U[ mesh->mesh.nb[id][mesh->lattice.reverse[j]] ][1]; */
-/* 		Uw[2] = mfields->U[ mesh->mesh.nb[id][mesh->lattice.reverse[j]] ][2]; */
-		
-	    
-/* 		// Move over lattice velocities */
-	    
-/* 		for( k = 0 ; k < mesh->mesh.Q ; k++ ) { */
-
-
-/* 		    if ( mesh->mesh.nb[id][k] == -1 ) { */
-
-		
-/* 			// Need density and velocity at neighbour (reverse) node */
-		    
-/* 			int nbid = mesh->mesh.nb[id][ mesh->lattice.reverse[k] ];		     */
-
-
-/* 			if( nbid != -1 ) { */
-
-/* 			    liMRTEquilibrium( &mesh->lattice, mfields->rho[nbid], mfields->U[nbid], f_eq_nb ); */
-
-/* 			    liMRTEquilibrium( &mesh->lattice, mfields->rho[nbid], Uw, f_eq_bnd ); */
-		    
-
-/* 			    // Update distribution */
-			
-/* 			    field->value[id][k] = f_eq_bnd[k] + (field->value[nbid][k] - f_eq_nb[k] );		     */
-
-/* 			} */
-		
-
-/* 		    } */
-
-/* 		} */
-
-
-/* 	    } */
-	    
-/* 	} */
-	
-	
-
-/*     } */
-
-/* } */
-
-
 
 void outflow( struct latticeMesh* mesh, struct macroFields* mfields, struct lbeField* field, unsigned int bid ) {
+
 
     unsigned int i, j, k;
     
@@ -103,36 +21,124 @@ void outflow( struct latticeMesh* mesh, struct macroFields* mfields, struct lbeF
 	int id = mesh->mesh.bd.bdPoints[bid][i];
 
 
-	// Move over principal directions only
+	// Check which is the normal direction, using unknown neighbours
 
-	for( j = 1 ; j <= 4 ; j++ ) {
+	int normal[3] = {0,0,0};
 
-	    
-	    // Copy from reverse point if neighbour is not present
-	    
-	    if( mesh->mesh.nb[id][j] == -1 ) {
+	for( k = 0 ; k < mesh->mesh.Q ; k++ ) {
 
+	    if( mesh->mesh.nb[id][ mesh->lattice.reverse[k] ] == -1 ) {
 
-		unsigned int nid = mesh->mesh.nb[id][ mesh->lattice.reverse[j] ];
-		
+		for( j = 0 ; j < 3 ; j++ ) {
 
-		// Move over lattice velocities and copy info
-	
-		for( k = 0 ; k < mesh->mesh.Q ; k++ ) {
-
-		    field->value[id][k] = field->value[nid][k];
+		    normal[j] += mesh->lattice.vel[k][j];
 
 		}
 
-		
-
 	    }
-	    
-       
 
 	}
+
+	for( j = 0 ; j < 3 ; j++ ) {
+
+	    if( normal[j] != 0 ) {
+
+		normal[j] = normal[j] / abs(normal[j]);
+
+	    }
+
+	}
+
+	
+	
+
+
+	// Detect correspondence with lattice velocities
+
+	unsigned int ln = 0;
+
+	for( k = 0 ; k < mesh->mesh.Q ; k++ ) {
+	    
+	    if(  (mesh->lattice.vel[k][0] == normal[0])  &&  (mesh->lattice.vel[k][1] == normal[1])  &&  (mesh->lattice.vel[k][2] == normal[2])  ) {	       
+		
+	    	ln = k;
+
+	    }
+
+	}
+	
+	
+
+	// Interpolate missing components from neighbour in normal direction
+
+	unsigned int neigh = mesh->mesh.nb[id][ln];
+
+	if( neigh != -1 ) {
+
+	    for( k = 0 ; k < mesh->mesh.Q ; k++ ) {
+
+		if( mesh->mesh.nb[id][k] == -1 ) {
+		
+		    field->value[id][k] = field->value[neigh][k];
+
+		}
+
+	    }
+
+	}
+	
+
 	
 
     }
 
 }
+
+
+
+
+/* void outflow( struct latticeMesh* mesh, struct macroFields* mfields, struct lbeField* field, unsigned int bid ) { */
+
+/*     unsigned int i, j, k; */
+    
+
+/*     // Move over boundary elements */
+    
+/*     for( i = 0 ; i < mesh->mesh.bd.nbdelem[bid] ; i++ ) { */
+
+/* 	int id = mesh->mesh.bd.bdPoints[bid][i]; */
+
+
+/* 	// Move over principal directions only */
+
+/* 	for( j = 1 ; j <= 4 ; j++ ) { */
+
+	    
+/* 	    // Copy from reverse point if neighbour is not present */
+	    
+/* 	    if( mesh->mesh.nb[id][j] == -1 ) { */
+
+
+/* 		unsigned int nid = mesh->mesh.nb[id][ mesh->lattice.reverse[j] ]; */
+		
+
+/* 		// Move over lattice velocities and copy info */
+	
+/* 		for( k = 0 ; k < mesh->mesh.Q ; k++ ) { */
+
+/* 		    field->value[id][k] = field->value[nid][k]; */
+
+/* 		} */
+
+		
+
+/* 	    } */
+	    
+       
+
+/* 	} */
+	
+
+/*     } */
+
+/* } */
