78a2ad52d31a4373db5a91fb5759dcd1339a737b
Ezequiel O. Fogliatto <ezefogliatto@gmail.com>
2018-04-19 11:51:22 -0300

    Trabajando con vstring

--- src/basic/vstring.c
+++ src/basic/vstring.c
@@ -1,9 +1,44 @@
 #include <vstring.h>
+#include <stdarg.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
 
 unsigned int vstring( char** strp, const char* fmt, ... ) {
 
     unsigned int status = 0;
 
+
+    // Work with arguments
+
+    va_list valist;
+
+    va_start( valist, fmt );
+
+
+    printf(fmt,valist);
+    
+    
+    // Compute size needed and allocate memory
+
+    size_t strl = snprintf(NULL, 0, fmt, valist) + 1;
+
+    /* printf("%d\n",(int)strl); */
+
+    char* aux = (char*)malloc( (strl+1) * sizeof(char) );
+
+    memset(strp, '\0', strl);
+
+
+    // Print fmt
+
+    /* strl = snprintf(aux, strl, fmt, valist); */
+
+    /* printf("%s\n",aux); */
+    
+
+    va_end(valist);
 
 
     return status;
--- src/test/test2.c
+++ src/test/test2.c
@@ -1,125 +1,135 @@
-#include <latticeModel.h>
-#include <io.h>
+#include <basic.h>
 
 int main() {
 
+    char* msg;
 
-    // Read model from file
-    char c[100];
-    lookUpStringEntry("input","LBModel", c);
+    char* fname = "fafa";
     
-    printf("\n");
-    printf("LBModel: %s\n",c);
-    printf("\n");
+    unsigned int status = vstring(&msg, "%s/Lambda", fname);
 
-
-    
-
-    // Lattice velocities
-    unsigned int i;    
-    int** lvel = latticeVelocities(c);
-
-    printf("\nVelocities\n");
-    for( i = 0 ; i < 9 ; i++) {
-
-    	printf(" %d  %d  %d\n",lvel[i][0],lvel[i][1],lvel[i][2]);
-	
-    }
-    printf("\n\n");
-
-
-
-
-    // Lattice weights    
-    double* omega = latticeWeights(c);
-
-    printf("\nWeights\n");
-    for( i = 0 ; i < 9 ; i++) {
-
-    	printf(" %f\n",omega[i]);
-	
-    }
-    printf("\n\n");    
-
-    
-
-    // Reverse directions
-    int* rev = latticeReverseDir(c);
-
-    printf("\nReverse dir\n");
-    for( i = 0 ; i < 9 ; i++) {
-
-    	printf(" %d\n",rev[i]);
-	
-    }
-    printf("\n\n");    
-
-
-
-    // MRT Matrix   
-    double** M = MRTMatrix(c);
-
-    printf("\nMRT\n");
-    int j;
-    for( i = 0 ; i < 9 ; i++) {
-
-	for( j = 0 ; j < 9 ; j++) {
-
-	    printf("%.0f ",M[i][j]);
-
-	}
-	
-	printf("\n");
-	
-    }
-    printf("\n\n");
-
-
-    
-
-    // MRT Inv Matrix   
-    double** invM = MRTInvMatrix(c);
-
-    printf("\nMRT Inv\n");
-    for( i = 0 ; i < 9 ; i++) {
-
-	for( j = 0 ; j < 9 ; j++) {
-
-	    printf("%f ",invM[i][j]);
-
-	}
-	
-	printf("\n");
-	
-    }
-    printf("\n\n");     
-    
-
-
-
-
-
-
-
-
-    // Pseudo pot w
-    double* w = ppWeights(c);
-
-    printf("\nPP W\n");
-    for( i = 0 ; i < 9 ; i++) {
-
-    	printf(" %f\n",w[i]);
-	
-    }
-    printf("\n\n"); 
-
-
-
-
-
-    printf("\ncs2: %f\n\n",cs2(c));
+    /* if(status) */
+    /* 	printf("%s\n",msg); */
     
     
     return 0;
 
 }
+
+
+
+    /* // Read model from file */
+    /* char c[100]; */
+    /* lookUpStringEntry("input","LBModel", c); */
+    
+    /* printf("\n"); */
+    /* printf("LBModel: %s\n",c); */
+    /* printf("\n"); */
+
+
+    
+
+    /* // Lattice velocities */
+    /* unsigned int i;     */
+    /* int** lvel = latticeVelocities(c); */
+
+    /* printf("\nVelocities\n"); */
+    /* for( i = 0 ; i < 9 ; i++) { */
+
+    /* 	printf(" %d  %d  %d\n",lvel[i][0],lvel[i][1],lvel[i][2]); */
+	
+    /* } */
+    /* printf("\n\n"); */
+
+
+
+
+    /* // Lattice weights     */
+    /* double* omega = latticeWeights(c); */
+
+    /* printf("\nWeights\n"); */
+    /* for( i = 0 ; i < 9 ; i++) { */
+
+    /* 	printf(" %f\n",omega[i]); */
+	
+    /* } */
+    /* printf("\n\n");     */
+
+    
+
+    /* // Reverse directions */
+    /* int* rev = latticeReverseDir(c); */
+
+    /* printf("\nReverse dir\n"); */
+    /* for( i = 0 ; i < 9 ; i++) { */
+
+    /* 	printf(" %d\n",rev[i]); */
+	
+    /* } */
+    /* printf("\n\n");     */
+
+
+
+    /* // MRT Matrix    */
+    /* double** M = MRTMatrix(c); */
+
+    /* printf("\nMRT\n"); */
+    /* int j; */
+    /* for( i = 0 ; i < 9 ; i++) { */
+
+    /* 	for( j = 0 ; j < 9 ; j++) { */
+
+    /* 	    printf("%.0f ",M[i][j]); */
+
+    /* 	} */
+	
+    /* 	printf("\n"); */
+	
+    /* } */
+    /* printf("\n\n"); */
+
+
+    
+
+    /* // MRT Inv Matrix    */
+    /* double** invM = MRTInvMatrix(c); */
+
+    /* printf("\nMRT Inv\n"); */
+    /* for( i = 0 ; i < 9 ; i++) { */
+
+    /* 	for( j = 0 ; j < 9 ; j++) { */
+
+    /* 	    printf("%f ",invM[i][j]); */
+
+    /* 	} */
+	
+    /* 	printf("\n"); */
+	
+    /* } */
+    /* printf("\n\n");      */
+    
+
+
+
+
+
+
+
+
+    /* // Pseudo pot w */
+    /* double* w = ppWeights(c); */
+
+    /* printf("\nPP W\n"); */
+    /* for( i = 0 ; i < 9 ; i++) { */
+
+    /* 	printf(" %f\n",w[i]); */
+	
+    /* } */
+    /* printf("\n\n");  */
+
+
+
+
+
+    /* printf("\ncs2: %f\n\n",cs2(c)); */
+
