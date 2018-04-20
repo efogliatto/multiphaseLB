f11d1e28ae0544b159132dd4c2933ed31d84f8d8
Ezequiel O. Fogliatto <ezefogliatto@gmail.com>
2018-04-20 09:55:34 -0300

    Adaptaci\'on de outflow

--- src/boundary/outflow/outflow.c
+++ src/boundary/outflow/outflow.c
@@ -3,12 +3,9 @@
 #include <latticeMesh.h>
 #include <lbeField.h>
 #include <macroFields.h>
-#include <liTempEquilibrium.h>
-#include <liMRTEquilibrium.h>
 
 
-
-void outflow(  latticeMesh* mesh,  macroFields* mfields,  double** field, unsigned int bid ) {
+void outflow( latticeMesh* mesh, macroFields* mfields, double** field, unsigned int bid ) {
 
 
     unsigned int i, j, k;
@@ -79,7 +76,7 @@ void outflow(  latticeMesh* mesh,  macroFields* mfields,  double** field, unsign
 
 		if( mesh->mesh.nb[id][k] == -1 ) {
 		
-		    field->value[id][k] = field->value[neigh][k];
+		    field[id][k] = field[neigh][k];
 
 		}
 
@@ -93,52 +90,3 @@ void outflow(  latticeMesh* mesh,  macroFields* mfields,  double** field, unsign
     }
 
 }
-
-
-
-
-/* void outflow( struct latticeMesh* mesh, struct macroFields* mfields, struct lbeField* field, unsigned int bid ) { */
-
-/*     unsigned int i, j, k; */
-    
-
-/*     // Move over boundary elements */
-    
-/*     for( i = 0 ; i < mesh->mesh.bd.nbdelem[bid] ; i++ ) { */
-
-/* 	int id = mesh->mesh.bd.bdPoints[bid][i]; */
-
-
-/* 	// Move over principal directions only */
-
-/* 	for( j = 1 ; j <= 4 ; j++ ) { */
-
-	    
-/* 	    // Copy from reverse point if neighbour is not present */
-	    
-/* 	    if( mesh->mesh.nb[id][j] == -1 ) { */
-
-
-/* 		unsigned int nid = mesh->mesh.nb[id][ mesh->lattice.reverse[j] ]; */
-		
-
-/* 		// Move over lattice velocities and copy info */
-	
-/* 		for( k = 0 ; k < mesh->mesh.Q ; k++ ) { */
-
-/* 		    field->value[id][k] = field->value[nid][k]; */
-
-/* 		} */
-
-		
-
-/* 	    } */
-	    
-       
-
-/* 	} */
-	
-
-/*     } */
-
-/* } */
