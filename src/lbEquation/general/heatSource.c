91c0f43913e4616612f69fd3789722628b7c74ec
Ezequiel O. Fogliatto <ezefogliatto@gmail.com>
2018-06-08 12:20:04 -0300

    Se prob\'o usar una aproximaci\'on a orden epsilon para grad(T), pero se ve que hay una diferencia con el c\'alculo por diferencias finitas. Quedan comentadas las l\'ineas probadas

--- src/lbEquation/general/heatSource.c
+++ src/lbEquation/general/heatSource.c
@@ -16,11 +16,20 @@ void heatSource( latticeMesh* mesh, macroFields* mfields, lbeField* field ) {
     double gradT[3],
 	gradRho[3];
 
+    /* double w = 0; */
+
+    /* double* n     = (double*)malloc( mesh->lattice.Q * sizeof(double) );   // m:  momentum space */
+
+
+    
 
     switch( field->model ){
 
     case myMRT:
 
+	/* w = 6.0 / (4.0 + 3.0 * field->lbparam.myMRT.alpha_1 + 2.0 * field->lbparam.myMRT.alpha_2 ); */
+
+	
 	for( id = 0 ; id < mesh->parallel.nlocal ; id++ ) {
 
 	    
@@ -30,6 +39,30 @@ void heatSource( latticeMesh* mesh, macroFields* mfields, lbeField* field ) {
 	    // Temperature gradient
     
 	    scalarGradient( gradT, mfields->T, mesh, id );
+
+
+	    ///////////////////////////////////////////////////////////////////
+	    //                        Version de prueba                      //
+	    ///////////////////////////////////////////////////////////////////
+
+	    
+	    /* // Equilibrium distribution */
+
+	    /* matVecMult(mesh->lattice.M, field->value[id], n, mesh->lattice.Q); */
+
+
+	    /* // Temperature gradient */
+	    
+	    /* gradT[0] = -w * ( field->lbparam.myMRT.Lambda[3] * (n[3] - mfields->T[id]*mfields->U[id][0])    +   0.5 * field->lbparam.myMRT.Lambda[3] * field->lbparam.myMRT.Lambda[4] * (n[4] + mfields->T[id]*mfields->U[id][0])); */
+
+	    /* gradT[1] = -w * ( field->lbparam.myMRT.Lambda[5] * (n[5] - mfields->T[id]*mfields->U[id][1])    +   0.5 * field->lbparam.myMRT.Lambda[5] * field->lbparam.myMRT.Lambda[6] * (n[4] + mfields->T[id]*mfields->U[id][1])); */
+
+	    /* gradT[2] = 0; */
+	    
+
+	    ///////////////////////////////////////////////////////////////////	    
+
+	    
 
 
 	    // 1/rho gradient
@@ -82,5 +115,11 @@ void heatSource( latticeMesh* mesh, macroFields* mfields, lbeField* field ) {
 	    
 
     }
+
+
+
+
+
+    /* free(n); */
 
 }
