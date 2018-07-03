e640b3c7e202c311dd8f33860303d2d4a7e09f72
Ezequiel O. Fogliatto <ezefogliatto@gmail.com>
2018-07-03 07:31:50 -0300

    Escritura en ensight binary para scalar field. Falta la paralelizaci\'on

--- src/IO/ensight/writeScalarToEnsight.c
+++ src/IO/ensight/writeScalarToEnsight.c
@@ -10,107 +10,211 @@
 void writeScalarToEnsight( char* fname, scalar* field, latticeMesh* mesh ) {
 
 
-    // Receive flag from "previous" processor
 
-    int flag;
-    
-    if(   ( mesh->parallel.pid != 0 )   &&  ( mesh->parallel.worldSize > 1 )   ) {
-    
-    	MPI_Recv(&flag, 1, MPI_INT, mesh->parallel.pid-1, mesh->parallel.pid-1, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+    // Non paralel but distributed
 
-    }
+    if( mesh->parallel.worldSize == 1 ) {
 
-
-    
-
-    /* // Count file ocurrences */
-
-    /* DIR *dir; */
-    
-    /* struct dirent *ent; */
-
-    /* uint fcount = 0; */
-
-    /* char name[256]; */
-
-    /* sprintf(name,"lattice.%s",fname); */
-
-
-    /* if ((dir = opendir (".")) != NULL) { */
-
-    /* 	while ((ent = readdir (dir)) != NULL) { */
-	    
-    /* 	    if(strstr(ent->d_name, name) != NULL) { */
-
-    /* 		fcount++; */
-
-    /* 	    } */
-	    
-    /* 	} */
 	
-    /* 	closedir (dir); */
-	
-    /* } */
-
-
-
-
-    // Write field
+	// Write field
     
-    FILE *outFile;
+	FILE *outFile;
 
-    char fileName[100];
+	char fileName[100];
 
-    uint fcount = timeToIndex(mesh->time.current);
+	uint fcount = timeToIndex(mesh->time.current);
 
-    sprintf(fileName, "lattice.%s_%d", fname, fcount);
+	sprintf(fileName, "lattice.%s_%d", fname, fcount);
     
 
-    if( mesh->parallel.pid == 0 ) {
+	// Header
 	
-	outFile = fopen(fileName, "w");
+	if( mesh->parallel.pid == 0 ) {
+	
+	    outFile = fopen(fileName, "wb");
+
+	    testFile(outFile, fileName);
+
+	    char* msg = (char*)malloc( 80*sizeof(char) );
+
+	    memset(msg,'\0', 80);	
+
+	    sprintf(msg, "%s", fname);
+
+	    fwrite( msg, sizeof(char), 80, outFile );	  
+
+	    fclose(outFile);
+
+	    free(msg);
+
+	}
+
+
+	// Part description
+
+	outFile = fopen(fileName, "ab");
 
 	testFile(outFile, fileName);
 
-	fprintf(outFile, "%s\n", fname);
+
+	char* msg = (char*)malloc( 80*sizeof(char) );
+
+	memset(msg,'\0', 80);	
+
+	sprintf(msg, "part");
+	
+	fwrite( msg, sizeof(char), 80, outFile );
+
+
+	
+	uint pid = mesh->parallel.pid+1;
+
+	fwrite( &pid, sizeof(int), 1, outFile );
+
+
+
+	memset(msg,'\0', 80);	
+
+	sprintf(msg, "coordinates");
+	
+	fwrite( msg, sizeof(char), 80, outFile );
+	
+
+
+    
+	uint i;
+
+	float value;
+    
+	for( i = 0 ; i < mesh->mesh.nPoints ; i++ ) {
+
+	    value = (float)field[i];
+	    
+	    fwrite( &value, sizeof(float), 1, outFile );
+	
+	}
+
 
 	fclose(outFile);
 
-    }
-
-
-    outFile = fopen(fileName, "a");
-
-    testFile(outFile, fileName);
-
-    fprintf(outFile,"part\n");
-
-    fprintf(outFile,"%10d\n",mesh->parallel.pid+1);
-
-    fprintf(outFile,"coordinates\n");
-
-
-    
-    uint ii;
-    
-    for( ii = 0 ; ii < mesh->mesh.nPoints ; ii++ ) {
+	free(msg);
 	
-    	fprintf(outFile, "%12.5e\n", field[ii]);
-	
-    }
 
-
-    fclose(outFile);
-
-
-
-    // Send flag to "next" processor
-
-    if(    ( mesh->parallel.worldSize > 1 )     &&    ( mesh->parallel.pid != mesh->parallel.worldSize - 1 )  ) {
-    
-    	MPI_Send(&flag, 1, MPI_INT, mesh->parallel.pid+1, mesh->parallel.pid, MPI_COMM_WORLD);
-
-    }
+    }  
     
 
 }
+
+
+
+
+
+/* // ASCII Version */
+
+/* void writeScalarToEnsight( char* fname, scalar* field, latticeMesh* mesh ) { */
+
+
+/*     // Receive flag from "previous" processor */
+
+/*     int flag; */
+    
+/*     if(   ( mesh->parallel.pid != 0 )   &&  ( mesh->parallel.worldSize > 1 )   ) { */
+    
+/*     	MPI_Recv(&flag, 1, MPI_INT, mesh->parallel.pid-1, mesh->parallel.pid-1, MPI_COMM_WORLD, MPI_STATUS_IGNORE); */
+
+/*     } */
+
+
+    
+
+/*     /\* // Count file ocurrences *\/ */
+
+/*     /\* DIR *dir; *\/ */
+    
+/*     /\* struct dirent *ent; *\/ */
+
+/*     /\* uint fcount = 0; *\/ */
+
+/*     /\* char name[256]; *\/ */
+
+/*     /\* sprintf(name,"lattice.%s",fname); *\/ */
+
+
+/*     /\* if ((dir = opendir (".")) != NULL) { *\/ */
+
+/*     /\* 	while ((ent = readdir (dir)) != NULL) { *\/ */
+	    
+/*     /\* 	    if(strstr(ent->d_name, name) != NULL) { *\/ */
+
+/*     /\* 		fcount++; *\/ */
+
+/*     /\* 	    } *\/ */
+	    
+/*     /\* 	} *\/ */
+	
+/*     /\* 	closedir (dir); *\/ */
+	
+/*     /\* } *\/ */
+
+
+
+
+/*     // Write field */
+    
+/*     FILE *outFile; */
+
+/*     char fileName[100]; */
+
+/*     uint fcount = timeToIndex(mesh->time.current); */
+
+/*     sprintf(fileName, "lattice.%s_%d", fname, fcount); */
+    
+
+/*     if( mesh->parallel.pid == 0 ) { */
+	
+/* 	outFile = fopen(fileName, "w"); */
+
+/* 	testFile(outFile, fileName); */
+
+/* 	fprintf(outFile, "%s\n", fname); */
+
+/* 	fclose(outFile); */
+
+/*     } */
+
+
+/*     outFile = fopen(fileName, "a"); */
+
+/*     testFile(outFile, fileName); */
+
+/*     fprintf(outFile,"part\n"); */
+
+/*     fprintf(outFile,"%10d\n",mesh->parallel.pid+1); */
+
+/*     fprintf(outFile,"coordinates\n"); */
+
+
+    
+/*     uint ii; */
+    
+/*     for( ii = 0 ; ii < mesh->mesh.nPoints ; ii++ ) { */
+	
+/*     	fprintf(outFile, "%12.5e\n", field[ii]); */
+	
+/*     } */
+
+
+/*     fclose(outFile); */
+
+
+
+/*     // Send flag to "next" processor */
+
+/*     if(    ( mesh->parallel.worldSize > 1 )     &&    ( mesh->parallel.pid != mesh->parallel.worldSize - 1 )  ) { */
+    
+/*     	MPI_Send(&flag, 1, MPI_INT, mesh->parallel.pid+1, mesh->parallel.pid, MPI_COMM_WORLD); */
+
+/*     } */
+    
+
+/* } */
