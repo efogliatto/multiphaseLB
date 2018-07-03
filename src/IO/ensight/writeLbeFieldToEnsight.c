5a2d5af93a1c923006e57d0a20381a74ed5ced68
Ezequiel O. Fogliatto <ezefogliatto@gmail.com>
2018-07-03 07:40:46 -0300

    Escritura de lbeField en ensight binary. Falta paralelizaci\'on

--- src/IO/ensight/writeLbeFieldToEnsight.c
+++ src/IO/ensight/writeLbeFieldToEnsight.c
@@ -11,126 +11,251 @@
 void writeLbeFieldToEnsight( char* fname, scalar** field, latticeMesh* mesh ) {
 
 
-    // Receive flag from "previous" processor
+    // Non paralel but distributed
 
-    int flag;
+    if( mesh->parallel.worldSize == 1 ) {
+
+
+	char* cname;
     
-    if(   ( mesh->parallel.pid != 0 )   &&  ( mesh->parallel.worldSize > 1 )   ) {
-    
-    	MPI_Recv(&flag, 1, MPI_INT, mesh->parallel.pid-1, mesh->parallel.pid-1, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+	uint status, k;
 
-    }
-
-
-    
-
-    char* cname;
-    
-    uint status, k;
-
-    for( k = 0 ; k < mesh->lattice.Q ; k++ ) {
+	for( k = 0 ; k < mesh->lattice.Q ; k++ ) {
 
 	
-	status = vstring(&cname, "%s%d", fname, k);
+	    status = vstring(&cname, "%s%d", fname, k);
 
-	if(status) {}
+	    if(status) {}
+	
 
-
-	/* // Count file ocurrences */
-
-	/* DIR *dir; */
+	
+	    // Write field
     
-	/* struct dirent *ent; */
+	    FILE *outFile;
 
-	/* uint fcount = 0; */
+	    char fileName[100];
 
-	/* char name[256]; */
+	    uint fcount = timeToIndex(mesh->time.current);
 
-	/* sprintf(name,"lattice.%s",cname); */
-
-
-	/* if ((dir = opendir (".")) != NULL) { */
-
-	/*     while ((ent = readdir (dir)) != NULL) { */
+	    sprintf(fileName, "lattice.%s_%d", cname, fcount);
 	    
-	/* 	if(strstr(ent->d_name, name) != NULL) { */
-
-	/* 	    fcount++; */
-
-	/* 	} */
-	    
-	/*     } */
-	
-	/*     closedir (dir); */
-	
-	/* } */
-
-
-
-
-	// Write field
     
-	FILE *outFile;
 
-	char fileName[100];
-
-	uint fcount = timeToIndex(mesh->time.current);
-
-	sprintf(fileName, "lattice.%s_%d", cname, fcount);
-
+	    // Header
 	
-
-	if( mesh->parallel.pid == 0 ) {
+	    if( mesh->parallel.pid == 0 ) {
 	
-	    outFile = fopen(fileName, "w");
+		outFile = fopen(fileName, "wb");
+
+		testFile(outFile, fileName);
+
+		char* msg = (char*)malloc( 80*sizeof(char) );
+
+		memset(msg,'\0', 80);	
+
+		sprintf(msg, "%s", fname);
+
+		fwrite( msg, sizeof(char), 80, outFile );	  
+
+		fclose(outFile);
+
+		free(msg);
+
+	    }
+
+
+	    // Part description
+
+	    outFile = fopen(fileName, "ab");
 
 	    testFile(outFile, fileName);
 
-	    fprintf(outFile, "%s\n", cname);
+
+	    char* msg = (char*)malloc( 80*sizeof(char) );
+
+	    memset(msg,'\0', 80);	
+
+	    sprintf(msg, "part");
+	
+	    fwrite( msg, sizeof(char), 80, outFile );
+
+
+	
+	    uint pid = mesh->parallel.pid+1;
+
+	    fwrite( &pid, sizeof(int), 1, outFile );
+
+
+
+	    memset(msg,'\0', 80);	
+
+	    sprintf(msg, "coordinates");
+	
+	    fwrite( msg, sizeof(char), 80, outFile );
+	
+
+
+    
+	    uint i;
+
+	    float value;
+    
+	    for( i = 0 ; i < mesh->mesh.nPoints ; i++ ) {
+
+		value = (float)field[i][k];
+	    
+		fwrite( &value, sizeof(float), 1, outFile );
+	
+	    }
+
 
 	    fclose(outFile);
 
+	    free(msg);
+
+
+
 	}
 
-
-	outFile = fopen(fileName, "a");
-
-	testFile(outFile, fileName);
-
-	fprintf(outFile,"part\n");
-
-	fprintf(outFile,"%10d\n",mesh->parallel.pid+1);
-
-	fprintf(outFile,"coordinates\n");
-
-	
-	uint i;
-    
-	for( i = 0 ; i < mesh->mesh.nPoints ; i++ ) {
-	
-	    fprintf(outFile, "%12.5e\n", field[i][k]);
-	
-	}
-
-
-	fclose(outFile);
-	
+	free(cname);
 	
 
     }
 
-
-    free(cname);
     
+  
 
-
-
-    // Send flag to "next" processor
-
-    if(    ( mesh->parallel.worldSize > 1 )     &&    ( mesh->parallel.pid != mesh->parallel.worldSize - 1 )  ){
-    
-    	MPI_Send(&flag, 1, MPI_INT, mesh->parallel.pid+1, mesh->parallel.pid, MPI_COMM_WORLD);
-
-    }
     
 }
+
+
+
+
+/* // ASCII VERSION */
+
+/* void writeLbeFieldToEnsight( char* fname, scalar** field, latticeMesh* mesh ) { */
+
+
+/*     // Receive flag from "previous" processor */
+
+/*     int flag; */
+    
+/*     if(   ( mesh->parallel.pid != 0 )   &&  ( mesh->parallel.worldSize > 1 )   ) { */
+    
+/*     	MPI_Recv(&flag, 1, MPI_INT, mesh->parallel.pid-1, mesh->parallel.pid-1, MPI_COMM_WORLD, MPI_STATUS_IGNORE); */
+
+/*     } */
+
+
+    
+
+/*     char* cname; */
+    
+/*     uint status, k; */
+
+/*     for( k = 0 ; k < mesh->lattice.Q ; k++ ) { */
+
+	
+/* 	status = vstring(&cname, "%s%d", fname, k); */
+
+/* 	if(status) {} */
+
+
+/* 	/\* // Count file ocurrences *\/ */
+
+/* 	/\* DIR *dir; *\/ */
+    
+/* 	/\* struct dirent *ent; *\/ */
+
+/* 	/\* uint fcount = 0; *\/ */
+
+/* 	/\* char name[256]; *\/ */
+
+/* 	/\* sprintf(name,"lattice.%s",cname); *\/ */
+
+
+/* 	/\* if ((dir = opendir (".")) != NULL) { *\/ */
+
+/* 	/\*     while ((ent = readdir (dir)) != NULL) { *\/ */
+	    
+/* 	/\* 	if(strstr(ent->d_name, name) != NULL) { *\/ */
+
+/* 	/\* 	    fcount++; *\/ */
+
+/* 	/\* 	} *\/ */
+	    
+/* 	/\*     } *\/ */
+	
+/* 	/\*     closedir (dir); *\/ */
+	
+/* 	/\* } *\/ */
+
+
+
+
+/* 	// Write field */
+    
+/* 	FILE *outFile; */
+
+/* 	char fileName[100]; */
+
+/* 	uint fcount = timeToIndex(mesh->time.current); */
+
+/* 	sprintf(fileName, "lattice.%s_%d", cname, fcount); */
+
+	
+
+/* 	if( mesh->parallel.pid == 0 ) { */
+	
+/* 	    outFile = fopen(fileName, "w"); */
+
+/* 	    testFile(outFile, fileName); */
+
+/* 	    fprintf(outFile, "%s\n", cname); */
+
+/* 	    fclose(outFile); */
+
+/* 	} */
+
+
+/* 	outFile = fopen(fileName, "a"); */
+
+/* 	testFile(outFile, fileName); */
+
+/* 	fprintf(outFile,"part\n"); */
+
+/* 	fprintf(outFile,"%10d\n",mesh->parallel.pid+1); */
+
+/* 	fprintf(outFile,"coordinates\n"); */
+
+	
+/* 	uint i; */
+    
+/* 	for( i = 0 ; i < mesh->mesh.nPoints ; i++ ) { */
+	
+/* 	    fprintf(outFile, "%12.5e\n", field[i][k]); */
+	
+/* 	} */
+
+
+/* 	fclose(outFile); */
+	
+	
+
+/*     } */
+
+
+/*     free(cname); */
+    
+
+
+
+/*     // Send flag to "next" processor */
+
+/*     if(    ( mesh->parallel.worldSize > 1 )     &&    ( mesh->parallel.pid != mesh->parallel.worldSize - 1 )  ){ */
+    
+/*     	MPI_Send(&flag, 1, MPI_INT, mesh->parallel.pid+1, mesh->parallel.pid, MPI_COMM_WORLD); */
+
+/*     } */
+    
+/* } */
