e321d154479fe6efd24df14f35a02330f1bffd3c
Ezequiel Fogliatto <fogliatto@cab.cnea.gov.ar>
2018-07-20 16:27:11 -0300

    Error en el c\'alculo de presi\'on para xuMRTModel. HAY QUE CORREGIR

--- src/lbEquation/xuMRTModel/xuMRTPressure.c
+++ src/lbEquation/xuMRTModel/xuMRTPressure.c
@@ -13,338 +13,338 @@ scalar xuMRTPressure( latticeMesh* mesh, lbeField* field, scalar* rho, scalar* T
 
 
     
-    // Laplacian of potential
+    /* // Laplacian of potential */
 
-    scalar lap = 0;
+    /* scalar lap = 0; */
 
-    int a;
+    /* int a; */
 
-    int b;
+    /* int b; */
 
-    int c;
+    /* int c; */
     
 
-    switch(mesh->lattice.model) {
+    /* switch(mesh->lattice.model) { */
 
 
-    case D3Q15:
+    /* case D3Q15: */
 
 
-    	// X - derivative
+    /* 	// X - derivative */
 
-    	a = mesh->mesh.nb[id][2];
+    /* 	a = mesh->mesh.nb[id][2]; */
 
-    	b = mesh->mesh.nb[id][1];
+    /* 	b = mesh->mesh.nb[id][1]; */
 
 	
-    	if(  (a != -1)  &&  (b != -1)  ) {
+    /* 	if(  (a != -1)  &&  (b != -1)  ) { */
     
-    	    lap = potential(mesh, rho[a], T[a])  +  potential(mesh, rho[b], T[b])  -  2 * potential(mesh, rho[id], T[id]);
+    /* 	    lap = potential(mesh, rho[a], T[a])  +  potential(mesh, rho[b], T[b])  -  2 * potential(mesh, rho[id], T[id]); */
 
-    	}
+    /* 	} */
 
-    	else {
+    /* 	else { */
 
-    	    if(  (a == -1)  &&  (b != -1)  ) {
+    /* 	    if(  (a == -1)  &&  (b != -1)  ) { */
 
-		c = mesh->mesh.nb[b][1];
+    /* 		c = mesh->mesh.nb[b][1]; */
 		
-		lap = potential(mesh, rho[c], T[c])  -   2 * potential(mesh, rho[b], T[b])  +  potential(mesh, rho[id], T[id]);
+    /* 		lap = potential(mesh, rho[c], T[c])  -   2 * potential(mesh, rho[b], T[b])  +  potential(mesh, rho[id], T[id]); */
 
-    	    }
+    /* 	    } */
 
-    	    else {
+    /* 	    else { */
 
-    		if(  (a != -1)  &&  (b == -1)  ) {
+    /* 		if(  (a != -1)  &&  (b == -1)  ) { */
     
-		    c = mesh->mesh.nb[a][2];
+    /* 		    c = mesh->mesh.nb[a][2]; */
 		
-		    lap = potential(mesh, rho[c], T[c])  -   2 * potential(mesh, rho[a], T[a])  +  potential(mesh, rho[id], T[id]);    
+    /* 		    lap = potential(mesh, rho[c], T[c])  -   2 * potential(mesh, rho[a], T[a])  +  potential(mesh, rho[id], T[id]);     */
 
-    		}
+    /* 		} */
 
-    		else {
+    /* 		else { */
 
-    		    lap = 0;
+    /* 		    lap = 0; */
 
-    		}
+    /* 		} */
 
-    	    }
+    /* 	    } */
 
-    	}
-
-
+    /* 	} */
 
 
-    	// Y - derivative
 
-    	a = mesh->mesh.nb[id][4];
 
-    	b = mesh->mesh.nb[id][3];
+    /* 	// Y - derivative */
+
+    /* 	a = mesh->mesh.nb[id][4]; */
+
+    /* 	b = mesh->mesh.nb[id][3]; */
 
 	
-    	if(  (a != -1)  &&  (b != -1)  ) {
+    /* 	if(  (a != -1)  &&  (b != -1)  ) { */
     
-	    lap = lap + potential(mesh, rho[a], T[a])  +  potential(mesh, rho[b], T[b])  -  2 * potential(mesh, rho[id], T[id]);
+    /* 	    lap = lap + potential(mesh, rho[a], T[a])  +  potential(mesh, rho[b], T[b])  -  2 * potential(mesh, rho[id], T[id]); */
 
-    	}
+    /* 	} */
 
-    	else {
+    /* 	else { */
 
-    	    if(  (a == -1)  &&  (b != -1)  ) {
+    /* 	    if(  (a == -1)  &&  (b != -1)  ) { */
     
-		c = mesh->mesh.nb[b][3];
+    /* 		c = mesh->mesh.nb[b][3]; */
 		
-		lap = lap + potential(mesh, rho[c], T[c])  -   2 * potential(mesh, rho[b], T[b])  +  potential(mesh, rho[id], T[id]);
+    /* 		lap = lap + potential(mesh, rho[c], T[c])  -   2 * potential(mesh, rho[b], T[b])  +  potential(mesh, rho[id], T[id]); */
 
-    	    }
+    /* 	    } */
 
-    	    else {
+    /* 	    else { */
 
-    		if(  (a != -1)  &&  (b == -1)  ) {
+    /* 		if(  (a != -1)  &&  (b == -1)  ) { */
     
-		    c = mesh->mesh.nb[a][4];
+    /* 		    c = mesh->mesh.nb[a][4]; */
 		
-		    lap = lap + potential(mesh, rho[c], T[c])  -   2 * potential(mesh, rho[a], T[a])  +  potential(mesh, rho[id], T[id]);    
+    /* 		    lap = lap + potential(mesh, rho[c], T[c])  -   2 * potential(mesh, rho[a], T[a])  +  potential(mesh, rho[id], T[id]);     */
 
-    		}
+    /* 		} */
 
-    		else {
-
-
-    		}
-
-    	    }
-
-    	}
+    /* 		else { */
 
 
+    /* 		} */
+
+    /* 	    } */
+
+    /* 	} */
 
 
 
 
-    	// Z - derivative
 
-    	a = mesh->mesh.nb[id][6];
 
-    	b = mesh->mesh.nb[id][5];
+    /* 	// Z - derivative */
+
+    /* 	a = mesh->mesh.nb[id][6]; */
+
+    /* 	b = mesh->mesh.nb[id][5]; */
 
 	
-    	if(  (a != -1)  &&  (b != -1)  ) {
+    /* 	if(  (a != -1)  &&  (b != -1)  ) { */
     
-	    lap = lap + potential(mesh, rho[a], T[a])  +  potential(mesh, rho[b], T[b])  -  2 * potential(mesh, rho[id], T[id]);
+    /* 	    lap = lap + potential(mesh, rho[a], T[a])  +  potential(mesh, rho[b], T[b])  -  2 * potential(mesh, rho[id], T[id]); */
 
-    	}
+    /* 	} */
 
-    	else {
+    /* 	else { */
 
-    	    if(  (a == -1)  &&  (b != -1)  ) {
+    /* 	    if(  (a == -1)  &&  (b != -1)  ) { */
     
-		c = mesh->mesh.nb[b][5];
+    /* 		c = mesh->mesh.nb[b][5]; */
 		
-		lap = lap + potential(mesh, rho[c], T[c])  -   2 * potential(mesh, rho[b], T[b])  +  potential(mesh, rho[id], T[id]);
+    /* 		lap = lap + potential(mesh, rho[c], T[c])  -   2 * potential(mesh, rho[b], T[b])  +  potential(mesh, rho[id], T[id]); */
 
-    	    }
+    /* 	    } */
 
-    	    else {
+    /* 	    else { */
 
-    		if(  (a != -1)  &&  (b == -1)  ) {
+    /* 		if(  (a != -1)  &&  (b == -1)  ) { */
     
-		    c = mesh->mesh.nb[a][6];
+    /* 		    c = mesh->mesh.nb[a][6]; */
 		
-		    lap = lap + potential(mesh, rho[c], T[c])  -   2 * potential(mesh, rho[a], T[a])  +  potential(mesh, rho[id], T[id]);    
+    /* 		    lap = lap + potential(mesh, rho[c], T[c])  -   2 * potential(mesh, rho[a], T[a])  +  potential(mesh, rho[id], T[id]);     */
 
-    		}
+    /* 		} */
 
-    		else {
+    /* 		else { */
 
 
-    		}
+    /* 		} */
 
-    	    }
+    /* 	    } */
 
-    	}
+    /* 	} */
 
 
 	
 	
 	
-	break;
+    /* 	break; */
 
 
 
-    default:
+    /* default: */
 
-	break;
-
-
-    }
+    /* 	break; */
 
 
-    scalar kappa = 0;
+    /* } */
 
-    if( field->lbparam.xuMRT.surfaceTension == xuSurfTen ) {
 
-	kappa = field->lbparam.xuMRT.kappa_st;
+    /* scalar kappa = 0; */
 
-    }
+    /* if( field->lbparam.xuMRT.surfaceTension == xuSurfTen ) { */
+
+    /* 	kappa = field->lbparam.xuMRT.kappa_st; */
+
+    /* } */
 
     
-    p = p   +   mesh->EOS.G * psi * lap * (1 + 2*kappa)/ 12;
+    /* p = p   +   mesh->EOS.G * psi * lap * (1 + 2*kappa)/ 12; */
 
 
 
 
 
 
-    // Gradient of potential
+    /* // Gradient of potential */
 
-    scalar grad[3] = {0,0,0};
+    /* scalar grad[3] = {0,0,0}; */
     
-    switch(mesh->lattice.model) {
+    /* switch(mesh->lattice.model) { */
 
-    case D3Q15:
+    /* case D3Q15: */
     
 
-    	// X - derivative
+    /* 	// X - derivative */
 
-    	a = mesh->mesh.nb[id][2];
-    	b = mesh->mesh.nb[id][1];
+    /* 	a = mesh->mesh.nb[id][2]; */
+    /* 	b = mesh->mesh.nb[id][1]; */
     
-    	if(  (a != -1)  &&  (b != -1)  ) {
+    /* 	if(  (a != -1)  &&  (b != -1)  ) { */
     
-    	    grad[0] = 0.5 * (potential(mesh, rho[a], T[a]) - potential(mesh, rho[b], T[b]));
+    /* 	    grad[0] = 0.5 * (potential(mesh, rho[a], T[a]) - potential(mesh, rho[b], T[b])); */
 
-    	}
+    /* 	} */
 
-    	else {
+    /* 	else { */
 
-    	    if(  (a == -1)  &&  (b != -1)  ) {
+    /* 	    if(  (a == -1)  &&  (b != -1)  ) { */
     
-    		grad[0] = (potential(mesh, rho[id], T[id]) - potential(mesh, rho[b], T[b]));
+    /* 		grad[0] = (potential(mesh, rho[id], T[id]) - potential(mesh, rho[b], T[b])); */
 
-    	    }
+    /* 	    } */
 
-    	    else {
+    /* 	    else { */
 
-    		if(  (a != -1)  &&  (b == -1)  ) {
+    /* 		if(  (a != -1)  &&  (b == -1)  ) { */
     
-    		    grad[0] = (potential(mesh, rho[a], T[a]) - potential(mesh, rho[id], T[id]));
+    /* 		    grad[0] = (potential(mesh, rho[a], T[a]) - potential(mesh, rho[id], T[id])); */
 
-    		}
+    /* 		} */
 
-    		else {
+    /* 		else { */
 
-    		    grad[0] = 0;
+    /* 		    grad[0] = 0; */
 
-    		}
+    /* 		} */
 
-    	    }
+    /* 	    } */
 
-    	}
-
-
+    /* 	} */
 
 
-    	// Y - derivative
 
-    	a = mesh->mesh.nb[id][4];
-    	b = mesh->mesh.nb[id][3];
+
+    /* 	// Y - derivative */
+
+    /* 	a = mesh->mesh.nb[id][4]; */
+    /* 	b = mesh->mesh.nb[id][3]; */
     
-    	if(  (a != -1)  &&  (b != -1)  ) {
+    /* 	if(  (a != -1)  &&  (b != -1)  ) { */
     
-    	    grad[1] = 0.5 * (potential(mesh, rho[a], T[a]) - potential(mesh, rho[b], T[b]));
+    /* 	    grad[1] = 0.5 * (potential(mesh, rho[a], T[a]) - potential(mesh, rho[b], T[b])); */
 
-    	}
+    /* 	} */
 
-    	else {
+    /* 	else { */
 
-    	    if(  (a == -1)  &&  (b != -1)  ) {
+    /* 	    if(  (a == -1)  &&  (b != -1)  ) { */
     
-    		grad[1] = (potential(mesh, rho[id], T[id]) - potential(mesh, rho[b], T[b]));
+    /* 		grad[1] = (potential(mesh, rho[id], T[id]) - potential(mesh, rho[b], T[b])); */
 
-    	    }
+    /* 	    } */
 
-    	    else {
+    /* 	    else { */
 
-    		if(  (a != -1)  &&  (b == -1)  ) {
+    /* 		if(  (a != -1)  &&  (b == -1)  ) { */
     
-    		    grad[1] = (potential(mesh, rho[a], T[a]) - potential(mesh, rho[id], T[id]));
+    /* 		    grad[1] = (potential(mesh, rho[a], T[a]) - potential(mesh, rho[id], T[id])); */
 
-    		}
+    /* 		} */
 
-    		else {
+    /* 		else { */
 
-    		    grad[1] = 0;
+    /* 		    grad[1] = 0; */
 
-    		}
+    /* 		} */
 
-    	    }
+    /* 	    } */
 
-    	}
-
-
+    /* 	} */
 
 
 
 
 
-    	// Z - derivative
 
-    	a = mesh->mesh.nb[id][6];
-    	b = mesh->mesh.nb[id][5];
+
+    /* 	// Z - derivative */
+
+    /* 	a = mesh->mesh.nb[id][6]; */
+    /* 	b = mesh->mesh.nb[id][5]; */
     
-    	if(  (a != -1)  &&  (b != -1)  ) {
+    /* 	if(  (a != -1)  &&  (b != -1)  ) { */
     
-    	    grad[2] = 0.5 * (potential(mesh, rho[a], T[a]) - potential(mesh, rho[b], T[b]));
+    /* 	    grad[2] = 0.5 * (potential(mesh, rho[a], T[a]) - potential(mesh, rho[b], T[b])); */
 
-    	}
+    /* 	} */
 
-    	else {
+    /* 	else { */
 
-    	    if(  (a == -1)  &&  (b != -1)  ) {
+    /* 	    if(  (a == -1)  &&  (b != -1)  ) { */
     
-    		grad[2] = (potential(mesh, rho[id], T[id]) - potential(mesh, rho[b], T[b]));
+    /* 		grad[2] = (potential(mesh, rho[id], T[id]) - potential(mesh, rho[b], T[b])); */
 
-    	    }
+    /* 	    } */
 
-    	    else {
+    /* 	    else { */
 
-    		if(  (a != -1)  &&  (b == -1)  ) {
+    /* 		if(  (a != -1)  &&  (b == -1)  ) { */
     
-    		    grad[2] = (potential(mesh, rho[a], T[a]) - potential(mesh, rho[id], T[id]));
+    /* 		    grad[2] = (potential(mesh, rho[a], T[a]) - potential(mesh, rho[id], T[id])); */
 
-    		}
+    /* 		} */
 
-    		else {
+    /* 		else { */
 
-    		    grad[2] = 0;
+    /* 		    grad[2] = 0; */
 
-    		}
+    /* 		} */
 
-    	    }
+    /* 	    } */
 
-    	}
+    /* 	} */
 
 
-	break;
+    /* 	break; */
 
 
 	
 
 
-    default:	
+    /* default:	 */
 
-	break;	
+    /* 	break;	 */
 	
 
-    }
+    /* } */
 
 
 
 
 
-    p = p + 2.0 * mesh->EOS.G * mesh->EOS.G * sigma * vectorMag2(grad);
+    /* p = p + 2.0 * mesh->EOS.G * mesh->EOS.G * sigma * vectorMag2(grad); */
     
     
 
