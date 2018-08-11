0e803d31e95ae16bd196944f12a502f87852c23c
Ezequiel Fogliatto <fogliatto@cab.cnea.gov.ar>
2018-08-11 13:56:59 -0300

    Incorporaci\'on de solver multiPhase3D para pruebas

---
+++ src/applications/solvers/multiPhase3D/multiPhase3D.c
@@ -0,0 +1,357 @@
+/*
+
+  Multiphase solver with heat transfer
+
+ */
+
+
+#include <io.h>
+#include <latticeModel.h>
+#include <basic.h>
+#include <mpi.h>
+#include <generalLbe.h>
+#include <pseudoPot.h>
+#include <omp.h>
+#include <checkMpArgs.h>
+#include <lbEquation.h>
+
+
+int main( int argc, char **argv ) {
+
+
+
+    // Check for arguments
+
+    mpOptions mp = checkMpArgs( argc, argv );
+    
+    
+
+    int pid, world;
+    
+    // Initialize mpi
+    MPI_Init(&argc, &argv);
+    MPI_Comm_rank(MPI_COMM_WORLD,&pid);
+    MPI_Comm_size(MPI_COMM_WORLD,&world);
+    
+    if(pid == 0) {
+    	printf("                    \n");
+    	printf("     o-----o-----o  \n");
+    	printf("     | -   |   - |  \n");
+    	printf("     |   - | -   |  \n");
+    	printf("     o<----o---->o       Two Phases - Lattice-Boltzmann solver with heat transfer. Fixed pseudopotential model\n");
+    	printf("     |   - | -   |  \n");
+    	printf("     | -   |   - |  \n");
+    	printf("     o-----o-----o  \n");
+    }
+
+
+
+    
+    // Simulation properties
+    
+    latticeMesh mesh = readLatticeMesh( pid );
+
+    mesh.lattice = setLatticeInfo();
+
+    mesh.EOS = readEOSInfo();
+
+    vtkInfo vtk = readVTKInfo();
+    
+
+    
+    
+
+    // Macroscopic fields
+    
+    macroFields mfields;
+    
+
+    
+    // Density
+
+    createScalarField( &mesh, &mfields.rho, "rho", MUST_READ);
+
+    mesh.EOS.rho_0 = averageScalarField(&mesh, mfields.rho);
+
+
+    
+    
+    // Velocity
+
+    createVectorField( &mesh, &mfields.U, 3, "U", MUST_READ);
+    
+
+    
+    // Temperature
+
+    createScalarField( &mesh, &mfields.T, "T", MUST_READ);
+
+
+
+    // Pressure
+
+    createScalarField( &mesh, &mfields.p, "p", MUST_READ);
+    
+    
+
+
+    // LBE fields
+
+    // Navier-Stokes field
+
+    lbeField f;
+    
+    createLbeField( &mesh, &f, "f", MUST_READ);
+
+    if(mp.frozen == 0) { f.update = 0; }
+
+
+    
+    // Energy field
+
+    lbeField g;
+    
+    createLbeField( &mesh, &g, "g", MUST_READ);
+
+    if(mp.ht == 0) { g.update = 0; }
+    
+    
+
+    // Initial equilibrium distribution
+   
+    equilibrium(&mesh, &mfields, &f);
+    
+    equilibrium(&mesh, &mfields, &g);
+
+
+
+
+
+    // Update macroscopic interaction force
+
+    mfields.Fi = matrixDoubleAlloc( mesh.mesh.nPoints, 3, -1 );
+    
+    interForce( &mesh, &mfields );
+    
+    syncVectorField( &mesh, mfields.Fi );
+
+
+    
+
+    // Heat source
+
+    heatSource( &mesh, &mfields, &g );
+
+    syncScalarField( &mesh, g.scalarSource );
+    
+   
+    if(pid == 0){printf("\n\n");}
+
+
+
+    
+    // Advance in time. Collide, stream, update and write
+    
+    while( updateTime(&mesh.time) ) {
+
+
+	    
+	// Collide f (Navier-Stokes)
+
+	xuMRTCollision( &mesh, &mfields, &f );
+
+
+		
+	// Collide g (Temperature)
+
+	myMRTCollision( &mesh, &mfields, &g );
+	
+	
+	
+	// Stream f
+	
+	lbstream( &mesh, &f );
+
+	
+	
+	// Stream g
+	
+	lbstream( &mesh, &g );
+
+
+	
+
+	// Apply boundary conditions
+	
+	updateBoundaries( &mesh, &mfields, &f );
+	
+	updateBoundaries( &mesh, &mfields, &g );
+
+
+	
+	
+	// Sync fields
+
+	if( mp.frozen != 0 ) {  syncPdfField( &mesh, f.value );  }
+
+	if( mp.ht != 0 ) {  syncPdfField( &mesh, g.value );  }
+
+	
+		
+
+
+	// Update macroscopic density
+
+	uint id;
+
+	for( id = 0 ; id < mesh.mesh.nPoints ; id++) {
+	    
+	    mfields.rho[id] = xuMRTDensity( &mesh, f.value[id] );
+	    
+	}
+	       
+
+
+	
+	
+	
+	// Update macroscopic temperature       
+
+	heatSource( &mesh, &mfields, &g );
+
+	syncScalarField( &mesh, g.scalarSource );
+
+	for( id = 0 ; id < mesh.mesh.nPoints ; id++) {
+
+	    mfields.T[id] = myMRTTemperature( &mesh, &mfields, &g, id );
+
+	}
+
+
+
+	
+	
+	// Update macroscopic velocity
+	
+	interForce( &mesh, &mfields );
+
+	syncVectorField( &mesh, mfields.Fi );
+
+	for( id = 0 ; id < mesh.mesh.nPoints ; id++) {
+	    
+	    xuMRTVelocity( &mesh, &mfields, &f, mfields.U, id );
+	    
+	}
+
+
+
+
+	
+	
+	
+    	// Write fields
+	
+    	if( writeFlag(&mesh.time) ) {
+
+	    
+    	    if(pid == 0) {
+		
+    		printf( "Time = %d\n", mesh.time.current );
+		
+    		printf("Elapsed time = %.2f seconds\n\n", elapsed(&mesh.time) );
+		
+    	    }
+
+
+	    
+	    // Update pressure
+
+	    macroPressure( &mesh, &mfields, &f );
+
+	    syncScalarField( &mesh, mfields.p );
+
+
+	    // Update average density
+	    
+	    mesh.EOS.rho_0 = averageScalarField(&mesh, mfields.rho);
+
+
+	    
+    	    // Write 
+
+	    if(mesh.time.data == pvtu) {
+	    
+		writeMeshToVTK( &mesh, &vtk );
+
+	    }
+
+	    if(mesh.time.data == ensight) {
+
+		updateCaseFile(&mesh);
+
+		MPI_Barrier(MPI_COMM_WORLD);
+
+	    }	    
+
+    	    writeScalarField( "rho", mfields.rho, &mesh );
+
+    	    writeScalarField( "T", mfields.T, &mesh );
+
+    	    writeScalarField( "p", mfields.p, &mesh );  
+
+    	    writeVectorField( "U", mfields.U, &mesh, 3 );
+
+    	    writeVectorField( "f", f.value, &mesh, mesh.lattice.Q );
+
+    	    writeVectorField( "g", g.value, &mesh, mesh.lattice.Q );
+
+
+	    if(mesh.time.data == pvtu) {
+	    
+		writePvtuExtra( &mesh, &vtk );
+
+		writeMainPvd();
+
+	    }
+
+
+	    
+	    
+    	}
+	
+
+    }
+
+
+    /* { */
+
+    /* 	uint ii; */
+
+    /* 	for( ii = 1 ; ii < mesh.parallel.nlocal ; ii+=3 ) { */
+
+    /* 	    printf(  "%d %g %g %g %g\n", mesh.mesh.points[ii][1], */
+    /* 		     mfields.Fi[ii][1], */
+    /* 		     p_eos(&mesh.EOS, mfields.rho[ii], mfields.T[ii]), */
+    /* 		     mfields.rho[ii], */
+    /* 		     potential( &mesh, mfields.rho[ii], mfields.T[ii] )); */
+
+    /* 	} */
+
+    /* } */
+
+    
+    // Print info
+    if(pid == 0) {
+	
+    	printf("\n  Finished in %.2f seconds \n\n", elapsed(&mesh.time) );
+	
+    }
+
+
+    
+    MPI_Finalize();
+
+    
+    return 0;
+    
+}
